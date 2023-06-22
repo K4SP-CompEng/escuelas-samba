351a2beb06e56a23bdcac2db003d5af6b4170be9
K4SP-ITeng <aseniorc@gmail.com>
2023-06-21 22:22:15 -0400

    Conexión de la BD con Laravel

--- resources/views/escuela.blade.php
+++ resources/views/escuela.blade.php
@@ -5,6 +5,6 @@
 @section('content')
     <p>Escuela: GRES Lorem Ipsum</p>
     <p>Breve historia: Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda nisi aliquid ullam impedit eligendi at voluptas velit, reiciendis rerum, esse quos odio dicta repellat. Dolores nesciunt similique temporibus consectetur a.</p>
-    <p>Bandera:</p>
+    <p>Colores: Amarillo, Azul, Rojo</p>
     <p></p>
 @endsection
